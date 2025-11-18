94b40162979d2b9512b3f7d471124ff604ac8556
keizercode <luqman.arohim@if.uai.ac.id>
2025-11-18 15:58:38 +0700

    fix services

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,11 +19,19 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-    {
-        // Force HTTPS in production
-        if ($this->app->environment('production')) {
-            URL::forceScheme('https');
-        }
+{
+    if ($this->app->environment('production')) {
+
+        // Memaksa Laravel pakai domain yg kita set
+        URL::forceRootUrl(config('app.url'));
+
+        // Memaksa pakai HTTPS
+        URL::forceScheme('https');
+
+        // Fix proxy / load balancer
+        request()->server->set('HTTP_X_FORWARDED_PROTO', 'https');
+        request()->server->set('HTTPS', 'on');
     }
-    
+}
+
 }
